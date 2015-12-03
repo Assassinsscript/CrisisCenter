13c5ed15e3357f6b6fd82d60642d1b2311ebc7e1
nvalaise <nicolaS.valaise@hotmail.fr>
2015-12-03 22:57:28 +0100

    relation crisis/headquarter

--- app/Crisis.php
+++ app/Crisis.php
@@ -9,4 +9,8 @@ class Crisis extends Model
     protected $fillable = array('name', 'begin_date', 'end_date', 'type', 'location');
 
     protected $dates = array('begin_date', 'end_date');
+
+    public function headquarter(){
+        return $this->belongsTo('App\Headquarter');
+    }
 }
