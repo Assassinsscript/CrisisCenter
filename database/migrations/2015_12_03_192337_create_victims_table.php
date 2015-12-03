b7e84ab4d14b30a5f472873b9208a471563eb1d1
Vincent Prigent <vincent.prigent@etu.u-bordeaux.fr>
2015-12-03 20:47:15 +0100

    Added gitignore storage

--- database/migrations/2015_12_03_192337_create_victims_table.php
+++ database/migrations/2015_12_03_192337_create_victims_table.php
@@ -14,6 +14,18 @@ class CreateVictimsTable extends Migration
     {
         Schema::create('victims', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('last_name');
+            $table->string('firs_name');
+            $table->string('gender');
+            $table->string('address');
+            $table->integer('postal_code');
+            $table->string('city');
+            $table->string('country');
+            $table->string('telephone1');
+            $table->string('telephone2');
+            $table->enum('blood_type', ['O-', 'A-', 'AB-', 'B-', 'B+', 'AB+', 'A+', 'O+']);
+            $table->date('birth_date');
+            $table->text('contraindication');
             $table->timestamps();
         });
     }
