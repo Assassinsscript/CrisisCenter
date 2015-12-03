4e4f0a15caf6139eb6a9ee158e50b0f861d6a691
Maxime Maheo <imaxame@gmail.com>
2015-12-03 21:12:09 +0100

    Add headquarters migration

--- database/migrations/2015_12_03_192613_create_support_center_table.php
+++ database/migrations/2015_12_03_192613_create_support_center_table.php
@@ -14,7 +14,15 @@ class CreateSupportCenterTable extends Migration
     {
         Schema::create('support_center', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('headquarters_id')->unsigned();
+            $table->string('name');
+            $table->string('gps_lat');
+            $table->string('gps_long');
             $table->timestamps();
+        });
+
+        Schema::base('support_center', function(Blueprint $table) {
+            $table->foreign('headquarters_id')->references('id')->on('headquarters')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
