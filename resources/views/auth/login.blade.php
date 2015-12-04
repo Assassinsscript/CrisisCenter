24fff15cff2708989b49800312edccc68d6f3e4f
Marty Lamoureux <marty@lamoureux-mail.fr>
2015-12-04 06:06:51 +0100

    even better :3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,28 +8,37 @@
     <div class="navy-bg lead p-xs m-t-lg text-center">
         CrisisCenter
     </div>
-    <div class="middle-box text-center loginscreen m-t-xl">
-        <div class="m-t-xl">
-            <p>
-                Veuillez vous identifier
-            </p>
+    <div class="container text-center ">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
 
-            @foreach ($errors->all() as $error)
-                <div class="alert alert-danger">
-                    {{ $error }}
-                </div>
-            @endforeach
+                <div class="m-t-xl">
+                    <p>
+                        Veuillez vous identifier
+                    </p>
 
-            {!! Form::open(['url' => 'auth/login', 'class' => 'm-t']) !!}
-                <div class="form-group">
-                    {!! Form::text('username', old('username'), ['placeholder' => 'Identifiant', 'class' => 'form-control']) !!}
-                </div>
-                <div class="form-group">
-                    {!! Form::password('password', ['placeholder' => 'Mot de Passe', 'class' => 'form-control']) !!}
-                </div>
-                {!! Form::submit('Connexion', ['class' => 'btn btn-primary block full-width m-b']) !!}
+                    @foreach ($errors->all() as $error)
+                        <div class="alert alert-danger">
+                            {{ $error }}
+                        </div>
+                    @endforeach
 
-            {!! Form::close() !!}
+                    {!! Form::open(['url' => 'auth/login', 'class' => 'm-t']) !!}
+                        <div class="form-group">
+                            {!! Form::text('username', old('username'), ['placeholder' => 'Identifiant', 'class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::password('password', ['placeholder' => 'Mot de Passe', 'class' => 'form-control']) !!}
+                        </div>
+                        {!! Form::submit('Connexion', ['class' => 'btn btn-primary block full-width m-b']) !!}
+
+                    <p>
+                        <b>Identifiants de connexion :</b> demo / demo
+                    </p>
+
+                    {!! Form::close() !!}
+                </div>
+            </div>
         </div>
     </div>
 
