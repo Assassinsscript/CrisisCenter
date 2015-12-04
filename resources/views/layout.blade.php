1707849c5a79a643079d97b407b9766fa4db0dc4
Maxime Maheo <imaxame@gmail.com>
2015-12-04 06:03:39 +0100

    tyj

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Gestion de crise">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" />
     <title>@yield('title', 'CrisisCenter') - CrisisCenter</title>
     <link href="{{ env('APP_ENV') == 'production' ? 'http://static.nuitinfo.martylamoureux.net/css/app.min.css' : asset('css/app.min.css') }}" rel="stylesheet">
 </head>
